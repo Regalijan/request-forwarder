cb861fe9c75a494c4da5db338da5b937f5927d68
Regalijan <72576136+Regalijan@users.noreply.github.com>
2024-12-21 03:26:04 +0000

    It works (I guess)

---
+++ index.js
@@ -0,0 +1,52 @@
+import { Server } from "@hapi/hapi";
+
+(async function () {
+  const server = new Server({
+    port: process.env.PORT || 8080,
+  });
+
+  server.route({
+    method: "*",
+    options: {
+      payload: {
+        maxBytes: 104857600,
+        parse: false,
+      },
+    },
+    path: "/{path*}",
+    handler: async function (request, h) {
+      const { headers, method, payload } = request;
+      const destination = headers["destination-host"];
+
+      if (!destination)
+        return h
+          .response('{"error":"No destination host provided"}')
+          .type("application/json")
+          .code(400);
+
+      delete headers["destination-host"];
+      delete headers["x-serverless-authorization"];
+
+      const apiResponse = await fetch(
+        `https://${destination}/${request.params.path}`,
+        {
+          body:
+            typeof payload === "undefined"
+              ? undefined
+              : Buffer.from(payload).toString("utf-8"),
+          headers,
+          method,
+        },
+      );
+
+      const response = h.response(await apiResponse.text());
+
+      for (const [name, value] of apiResponse.headers.entries())
+        response.header(name, value);
+
+      return response;
+    },
+  });
+
+  await server.start();
+})();
